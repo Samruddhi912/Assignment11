c3b9380bd836c1e3de59abb8ea6ad6c3b5e0a30c
Samruddhi <samruddhikadam09@gmail.com>
2024-12-03 01:02:23 +0530

    Assignment11

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { InvalidComponent } from './invalid/invalid.component';
+
+export const routes: Routes = [
+    {
+        path:'',
+        component:TechnologiesComponent
+    },
+    {
+        path:'books',
+        component:BooksComponent
+    },
+    {
+        path:'Technologies',
+        component:TechnologiesComponent
+    },
+    {
+        path:'**',
+        component:InvalidComponent
+    }
+];
